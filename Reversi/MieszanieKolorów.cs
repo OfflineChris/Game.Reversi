164236bdea9a5fa92813077ddbee4a2b3a2ffa25
Krzysztof Nowicki <krzy.novicki@gmail.com>
2024-01-28 22:34:29 +0100

    Dodaj pliki projektów.

---
+++ Reversi/MieszanieKolorów.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Media;
+
+namespace Reversi
+{
+    static class MieszanieKolorów
+    {
+        public static Color Lerp(this Color kolor, Color innyKolor, double waga)
+        {
+            byte r = (byte)(waga * kolor.R + (1 - waga) * innyKolor.R);
+            byte g = (byte)(waga * kolor.G + (1 - waga) * innyKolor.G);
+            byte b = (byte)(waga * kolor.B + (1 - waga) * innyKolor.B);
+            return Color.FromRgb(r, g, b);  
+        }
+        public static SolidColorBrush Lerp(this SolidColorBrush pędzel, SolidColorBrush innyPędziel, double waga)
+        {
+            return new SolidColorBrush(Lerp(innyPędziel.Color, innyPędziel.Color, waga));
+        }
+    }
+}
